5b8eb42a58d8536f5f9e6e783ce249cca428b042
michele de falco <defalco_michele@libero.it>
2016-05-05 14:36:46 +0200

    Create footer.php

---
+++ administrator/footer/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+<ul class="inline">
+<li>@2016</li>
+<li>CMS&nbsp;Bootstrap&nbsp;Blog by <a href="#">@micheledefalco</a></li>
+<li><a href="../logaut.php?idut=<?php echo $cod_md5; ?>"><i class="fa fa-sign-out"></i>&nbsp;Esci</a></li>
+<li><a href="../administrator/?idut=<?php echo $cod_md5; ?>&profile_use"><i class="fa fa-male"></i>&nbsp;Modifica Profilo</a></li>
+</ul>
+</div>
