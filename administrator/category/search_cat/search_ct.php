158f73de8f6ee6359660946c0a52998f92a99a27
michele de falco <defalco_michele@libero.it>
2016-05-05 14:31:35 +0200

    Create search_ct.php

---
+++ administrator/category/search_cat/search_ct.php
@@ -0,0 +1,34 @@
+<?php
+require_once('../../../connect.php');
+$myconn = @mysqli_connect(DB_HOST,DB_USER,DB_PSW,DB_NAME) or die("Errore Connessione: <b>" .mysqli_connect_error()."</b>");
+@mysqli_query($myconn," SET names 'UTF8' ");
+include("../../../lib.php");
+
+$idut = $_GET['idut'];
+$q = $_POST['q'];
+
+$idut = $_GET['idut'];
+$q = trim($_POST['q']);
+$lung_query = strlen($q);
+
+if( $lung_query!=0 ){
+$arrquery = array_unique(explode(" ", $q));
+
+if( count($arrquery)!=0 ){
+$newquery = implode("+",$arrquery);	
+header("Location: ../../../administrator/?idut=".$idut."&category=all&search=".$newquery);
+
+}
+else{
+header("Location: ../../../administrator/?idut=".$idut."&category=all&search=".$q);
+}
+}
+else{
+header("Location: ../../../administrator/?idut=".$idut."&category=all");	
+	
+}
+
+
+
+
+?>
