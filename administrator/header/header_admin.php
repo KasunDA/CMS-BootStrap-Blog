00f3f2fd5815a923339db5e38f854ed2499fd29f
michele de falco <defalco_michele@libero.it>
2016-05-05 14:37:44 +0200

    Create header_admin.php

---
+++ administrator/header/header_admin.php
@@ -0,0 +1,11 @@
+<div class="row-fluid">
+<div class="span12">
+<div class="span6">
+<h4>
+<a class="btn btn-mini radius" href="javascript:history.back()"><i class="fa fa-arrow-left"></i></a> 
+<a class="btn btn-mini radius" href="javascript:history.forward()"><i class="fa fa-arrow-right"></i></a>
+<i class="fa fa-play-circle"></i>&nbsp;CMS&nbsp;Bootstrap&nbsp;Blog
+</h4> 
+</div>
+</div>
+</div>  
