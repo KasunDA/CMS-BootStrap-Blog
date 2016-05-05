697152e5ff69d8a60bad81c2a9d712cbe79583ad
michele de falco <defalco_michele@libero.it>
2016-05-05 14:52:14 +0200

    Create blog_title.php

---
+++ blog_title/blog_title.php
@@ -0,0 +1,6 @@
+<?php
+$title_blog = $rowsetting['title_blog'];
+$st_title_blog = $rowsetting['st_title_blog'];
+?>
+<h1 class="blog-title"><?php echo $title_blog; ?></h1>
+<p class="blog-description"><?php echo $st_title_blog; ?></p>
