a19d2f7f60f85fcfe382bfdbe2a0928e8edce0d2
michele de falco <defalco_michele@libero.it>
2016-05-06 11:07:59 +0200

    Create footer_blog.php

---
+++ footer/footer_blog.php
@@ -0,0 +1,12 @@
+<?php
+$footer =  html_entity_decode($rowsetting['footer_blog']);
+$anncopy = date('Y');
+?>
+<footer class="blog-footer">
+<p><span class="copy">@<?php echo $anncopy; ?></span>&nbsp;
+<span class="desc_copy"><?php echo $footer; ?></span>
+</p>
+<p><a href="/?cookie-policy">cookie policy</a></p>
+<p><a href="" id="top">Torna su</a></p>
+
+</footer>
