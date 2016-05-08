53cc758011027a528f4803df45fc8151b7f4285c
michele de falco <defalco_michele@libero.it>
2016-05-08 14:31:37 +0200

    Delete create_connect.php

--- installazione/create_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connect = fopen("../connessione.php", "a");
-$dati_riga = "<?php\ndefine('DB_HOST','127.0.0.1');\ndefine('DB_USER','root');\ndefine('DB_PSW','');\ndefine('DB_NAME','micheledef');\n?>";
-fwrite($connect,$dati_riga);
-fclose($connect);
-?>
