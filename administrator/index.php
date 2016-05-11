23bae2bc299382d2822cd9fe7f24bde7ab99d02d
michele de falco <defalco_michele@libero.it>
2016-05-11 12:13:04 +0200

    Update index.php

--- administrator/index.php
+++ administrator/index.php
@@ -52,8 +52,10 @@ if( $st_themes=="attivo" ){
 <link href="../assets/css/bootstrap-datepicker.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link href="../assets/css/style.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
-<script src="../assets/js/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 </head>
 
