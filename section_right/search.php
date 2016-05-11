4667f750403cdb9c02a0d4a32353e7e99d253460
michele de falco <defalco_michele@libero.it>
2016-05-11 12:19:30 +0200

    Update search.php

--- section_right/search.php
+++ section_right/search.php
@@ -1,7 +1,7 @@
-<form action="/section_right/search_query.php" class="input-block-level"  method="post">
+<form action="/section_right/search_query.php" class="form-search"  method="post">
 <fieldset>
 <legend>Cerca nel Blog</legend>
-<p><input type="text"  class="" placeholder="Cerca...." name="query"></p>
+<p><input type="text"  class="input-block-level" placeholder="Cerca...." name="query"></p>
 <p><input type="submit" class="btn btn-primary" value="Vai"></p>
 </fieldset>
 </form>
