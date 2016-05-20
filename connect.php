ad9ab8c2c4a9382a7056d6687a71ae7974c07974
michele de falco <defalco_michele@libero.it>
2016-05-20 14:30:49 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,5 +3,5 @@ define('DB_HOST','localhost');
 define('DB_USER','michele74');
 define('DB_PSW','YtrEWQ1974');
 define('DB_NAME','mydata');
-define('PATH_NAME','C:\Programmi\EasyPHP-DevServer-14.1VC9\data\localweb');
+define('PATH_NAME','mypath');
 ?>
