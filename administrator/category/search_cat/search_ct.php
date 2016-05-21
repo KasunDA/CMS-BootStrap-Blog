88a4bc94277f3031aaaadad03660dfbd1234e8ed
michele de falco <defalco_michele@libero.it>
2016-05-21 14:11:26 +0200

    Add files via upload

--- administrator/category/search_cat/search_ct.php
+++ administrator/category/search_cat/search_ct.php
@@ -1,14 +1,10 @@
 <?php
-require_once('../../../connect.php');
-function mypath ($pathname , $file) {
-$file_include =  $pathname."/".$file;
-return $file_include;	
-};
+define('__ROOT__', dirname( dirname (  dirname ( dirname (__FILE__ ) )  ) ) ); 
+require_once(__ROOT__.'/connect.php');
 
 $myconn = @mysqli_connect(DB_HOST,DB_USER,DB_PSW,DB_NAME) or die("Errore Connessione: <b>" .mysqli_connect_error()."</b>");
 @mysqli_query($myconn," SET names 'UTF8' ");
-
-include( mypath( PATH_NAME , "lib.php" ) );
+include( __ROOT__."/lib.php" );
 
 $idut = $_GET['idut'];
 $q = $_POST['q'];
