a1ca128887c6b3c14bf19a78f80bc9352c1edca0
michele de falco <defalco_michele@libero.it>
2016-05-21 14:14:44 +0200

    Add files via upload

--- administrator/page_use/page_use.php
+++ administrator/page_use/page_use.php
@@ -1,74 +1,73 @@
 <?php
 
-
 if( isset($_GET['p_use']) ){    
-include( mypath( PATH_NAME , 'administrator/article/all_article.php' ) );      
+include( __ROOT__.'/administrator/article/all_article.php' );      
 }
 if( isset($_GET['mod_art']) ){
-include(  mypath( PATH_NAME , 'administrator/article/mod_art.php' ) );     
+include(  __ROOT__.'/administrator/article/mod_art.php' );     
 }
 if ( isset($_GET['profile_use']) ){
-include( mypath( PATH_NAME , 'administrator/utenti/profile_use.php' ) );     
+include( __ROOT__.'/administrator/utenti/profile_use.php' );     
 }
 if( isset($_GET['new_article']) ){    
-include( mypath( PATH_NAME , 'administrator/article/new_article.php' ) );      
+include( __ROOT__.'/administrator/article/new_article.php' );      
 }
 if( isset($_GET['category']) ){
-include( mypath( PATH_NAME , 'administrator/category/all_category.php' ) );		
+include( __ROOT__.'/administrator/category/all_category.php' );		
 }
 
 if( isset($_GET['mod_category']) ){
-include( mypath( PATH_NAME , 'administrator/category/mod_category.php' ) );		
+include( __ROOT__.'/administrator/category/mod_category.php' );		
 }
 
 if( isset($_GET['new_category']) ){
-include( mypath( PATH_NAME , 'administrator/category/new_category.php' ) );		
+include( __ROOT__.'/administrator/category/new_category.php' );		
 }
 
 if( isset($_GET['users']) ){
-include( mypath( PATH_NAME , 'administrator/utenti/all_users.php' ) );	
+include( __ROOT__.'/administrator/utenti/all_users.php' );	
 	
 }
 
 if( isset($_GET['mod_user']) ){
-include( mypath( PATH_NAME , 'administrator/utenti/mod_user.php' ) );		
+include( __ROOT__.'/administrator/utenti/mod_user.php' );		
 }
 
 if( isset($_GET['new_users']) ){
-include(  mypath( PATH_NAME , 'administrator/utenti/new_users.php' ) );		
+include(  __ROOT__.'/administrator/utenti/new_users.php' );		
 }
 
 
 if( isset($_GET['mod_cat_admin']) ){
-include( mypath( PATH_NAME , 'administrator/utenti/type_cat_admin.php' ) );		
+include( __ROOT__.'/administrator/utenti/type_cat_admin.php' );		
 }
 
 if( isset($_GET['menu']) ){
-include(  mypath( PATH_NAME , 'administrator/menu/all_menu.php' ) );		
+include(  __ROOT__.'/administrator/menu/all_menu.php' );		
 }
 
 if( isset($_GET['mod_menu']) ){
-include( mypath( PATH_NAME , 'administrator/menu/mod_menu.php' ) );		
+include( __ROOT__.'/administrator/menu/mod_menu.php' );		
 }
 if( isset($_GET['send_email']) ){
-include( mypath( PATH_NAME , 'administrator/utenti/send_email.php' ) );		
+include( __ROOT__.'/administrator/utenti/send_email.php' );		
 }
 
 if( isset($_GET['template']) ){
-include( mypath( PATH_NAME , 'administrator/template/option_temp.php' ) );		
+include( __ROOT__.'/administrator/template/option_temp.php' );		
 }
 
 
 if( isset($_GET['setting']) ){
-include( mypath( PATH_NAME , 'administrator/setting/all_config.php' ) );		
+include( __ROOT__.'/administrator/setting/all_config.php' );		
 }
 
 if( isset($_GET['comment']) ){
-include(  mypath( PATH_NAME , 'administrator/article/comment/comment.php' ) );		
+include(  __ROOT__.'/administrator/article/comment/comment.php' );		
 }
 
 if( isset($_GET['images']) ){
-include(  mypath( PATH_NAME , 'administrator/cont_images/cont_images.php' ) );		
+include(  __ROOT__.'/administrator/cont_images/cont_images.php' );		
 }
 
 
@@ -94,7 +93,7 @@ if( !isset($_GET['p_use']) &&
 	
 	
 	){
-include( mypath( PATH_NAME , 'administrator/include_home/home.php' ) );
+include( __ROOT__.'/administrator/include_home/home.php' );
 }
 
 
