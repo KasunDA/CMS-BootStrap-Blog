36a3e8f7ed19306ce4f9709e59633f5bbacecc70
michele de falco <defalco_michele@libero.it>
2016-05-22 21:55:13 +0200

    Update upload_image.php

--- administrator/cont_images/upload_image.php
+++ administrator/cont_images/upload_image.php
@@ -2,11 +2,14 @@
 define('__ROOT__', dirname( dirname (  dirname ( __FILE__  )  ) ) ); 
 require_once(__ROOT__.'/connect.php');
 
+if( isset($_GET['idut']) ){
+$idut = $_GET['idut'];
+
 $myconn = @mysqli_connect(DB_HOST,DB_USER,DB_PSW,DB_NAME) or die("Errore Connessione: <b>" .mysqli_connect_error()."</b>");
 @mysqli_query($myconn," SET names 'UTF8' ");
 
 if (!isset($_FILES['userfile']) || !is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-  echo "<span class=\"text-info\"><i class=\"fa fa-info-circle\" aria-hidden=\"true\"></i> Non hai inviato nessun file...</span>";
+  header("Location: ../../../administrator/?idut=".$idut."&images=_not_file"); 
   exit;    
 }
 
@@ -19,7 +22,7 @@ $url_image = "/../images/".$userfile_name;
 
 $target_file = $uploaddir.$_FILES['userfile']['name'];
 if (file_exists($target_file)) {
-  echo "<span class=\"text-info\"><i class=\"fa fa-info-circle\" aria-hidden=\"true\"></i> Il file esiste già.!</span>";
+  header("Location: ../../../administrator/?idut=".$idut."&images=_exist_file");
   exit;
 }
 
@@ -27,15 +30,15 @@ $ext_ok = array('jpg', 'gif', 'png');
 $temp = explode('.', $_FILES['userfile']['name']);
 $ext = end($temp);
 if (!in_array($ext, $ext_ok)) {
-  echo "<span class=\"text-error\"><i class=\"fa fa-exclamation-circle\" aria-hidden=\"true\"></i> Il file ha un estensione non ammessa! Usa solo immagini <em>.gif</em>, <em>.jpg</em> e <em>.png</em>.</span>";
-  exit;
+  header("Location: ../../../administrator/?idut=".$idut."&images=_not_extension");
+   exit;
 }
 
 
 $is_img = getimagesize($_FILES['userfile']['tmp_name']);
 if (!$is_img) {
-  echo "<span class=\"text-error\"><i class=\"fa fa-exclamation-circle\" aria-hidden=\"true\"></i> Puoi inviare solo immagini.</span>";
-  exit;    
+header("Location: ../../../administrator/?idut=".$idut."&images=_not_image");
+exit;    
 }
 
 
@@ -43,17 +46,16 @@ if ( move_uploaded_file($userfile_tmp, $uploaddir.$userfile_name) ) {
 
 $sql_insert_img = "INSERT INTO images ( name_img, url_img ) VALUES ('$userfile_name', '$url_image') ";
 $rs_insert_img = @mysqli_query($myconn,$sql_insert_img) or die( "Errore....".mysqli_error($myconn) );
-
-echo "<span class=\"text-success\"><i class=\"fa fa-check\" aria-hidden=\"true\"></i> Immagine caricata con successo</span>";
+header("Location: ../../../administrator/?idut=".$idut."&images=_success");
 }
 else{
-echo "<span class=\"text-error\"><i class=\"fa fa-exclamation-circle\" aria-hidden=\"true\"></i> Si è verificato un errore durante l'upload dell'immagine, riprova!</span>"; 
+header("Location: ../../../administrator/?idut=".$idut."&images=_error");
+
+
 }	
 	
 	
 }
 
-
-
-
+}
 ?>
