8417183fb987431e9d898f6564860c3a3ff2de44
michele de falco <defalco_michele@libero.it>
2016-05-22 14:59:41 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST','localhost');
-define('DB_USER','michele74');
-define('DB_PSW','YtrEWQ1974');
+define('DB_USER','myusername');
+define('DB_PSW','mypassword');
 define('DB_NAME','mydata');
 ?>
