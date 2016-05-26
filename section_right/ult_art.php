5eefa4b23019c3c922f85d1d12a7a3c5ab6e11ec
michele de falco <defalco_michele@libero.it>
2016-05-26 14:57:34 +0200

    Add files via upload

--- section_right/ult_art.php
+++ section_right/ult_art.php
@@ -5,8 +5,22 @@ if($num_art_vis_no_arch ==0){
 }
 else{
 
+$art_rec = fopen("administrator/setting/art_rec.txt","r");
+$cat_art_rec = fgets($art_rec,1024) ;
+
+if($cat_art_rec == "all"){
 $sqlultart = " SELECT * FROM articoli WHERE visibility = \"Si\" AND archiviato = \"No\" ORDER BY datacreate  DESC LIMIT 0,6" ;
-$rsultart = @mysqli_query($myconn,$sqlultart) or die( "Errore....".mysqli_error($myconn) );
+$rsultart = @mysqli_query($myconn,$sqlultart) or die( "Errore....".mysqli_error($myconn) );	
+}
+else{
+	
+$sqlultart = " SELECT * FROM articoli WHERE visibility = \"Si\" AND archiviato = \"No\" AND categoria = \"$cat_art_rec\" ORDER BY datacreate  DESC LIMIT 0,6" ;
+$rsultart = @mysqli_query($myconn,$sqlultart) or die( "Errore....".mysqli_error($myconn) );	
+	
+}
+
+
+
 	
 	
 echo "<ol class=\"list-unstyled\">";	
