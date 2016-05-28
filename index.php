c875ccd3afdf0eb43bfa2c1b9022b5901867bb80
michele de falco <defalco_michele@libero.it>
2016-05-28 13:44:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if( $st_themes=="attivo" ){
 <link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
 <link rel="stylesheet" href="../fontawesome/font-awesome/css/font-awesome.min.css">
 <style>
-body{margin-bottom:180px;}
+body{margin-bottom:220px;}
  
 @media (min-width: 600px) {
   .blog-cont{margin-top:100px;}
