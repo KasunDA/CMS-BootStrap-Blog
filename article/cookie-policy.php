f70f563a7919d99b5323afe66e4c937ea0b21792
michele de falco <defalco_michele@libero.it>
2016-06-09 22:22:47 +0200

    Update cookie-policy.php

--- article/cookie-policy.php
+++ article/cookie-policy.php
@@ -1,6 +1,6 @@
 <div class="blog-post">
 <?php
-$cookie_policy = $rowsetting['cookie_policy'];
+$cookie_policy = html_entity_decode( $rowsetting['cookie_policy'] , ENT_QUOTES, "UTF-8");
 echo $cookie_policy;
 ?>
 </div>
