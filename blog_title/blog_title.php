564f8ebd122a1ab957315504cd1707edf5125a52
michele de falco <defalco_michele@libero.it>
2016-06-09 22:29:21 +0200

    Update blog_title.php

--- blog_title/blog_title.php
+++ blog_title/blog_title.php
@@ -1,6 +1,6 @@
 <?php
-$title_blog = $rowsetting['title_blog'];
-$st_title_blog = $rowsetting['st_title_blog'];
+$title_blog = html_entity_decode($rowsetting['title_blog'], ENT_QUOTES, "UTF-8");
+$st_title_blog = html_entity_decode($rowsetting['st_title_blog'], ENT_QUOTES, "UTF-8");
 ?>
 <h1 class="blog-title"><?php echo $title_blog; ?></h1>
 <p class="blog-description"><?php echo $st_title_blog; ?></p>
